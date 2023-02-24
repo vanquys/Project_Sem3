13a504a6310b778a62b2c5d5964e004d382c793e
Van Quy <quyvan2003@gmail.com>
2023-02-24 22:24:24 +0700

    push login and competition

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,18 +13,8 @@ namespace Project_Sem3.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        
 
-            return View();
-        }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ assets/js/Competition.js
@@ -0,0 +1,21 @@
+﻿
+document.addEventListener('DOMContentLoaded', () => {
+    var closebtns = document.getElementsByClassName("close-btn");
+    var rgtbtns = document.getElementsByClassName("cp-btn-rgt");
+    var dialog = document.querySelector('.dialog');
+    var i;
+
+    for (i = 0; i < closebtns.length; i++) {
+        closebtns[i].addEventListener("click", function () {
+
+            dialog.classList.add('hidden')
+        });
+    }
+
+
+    for (i = 0; i < rgtbtns.length; i++) {
+        rgtbtns[i].addEventListener("click", function () {
+            dialog.classList.remove('hidden')
+        });
+    }
+});
