b78ffb23aad9428e82e1873bd1fe99c1beabb6e9
Van Quy <quyvan2003@gmail.com>
2023-03-25 17:55:36 +0700

    update

--- assets/js/AdFAQ.js
+++ assets/js/AdFAQ.js
@@ -1,4 +1,6 @@
-﻿document.addEventListener('DOMContentLoaded', () => {
+﻿
+document.addEventListener('DOMContentLoaded', () => {
+    var btnEdit = document.querySelectorAll('.btn-edit-faqs');
     const dialog = document.querySelector('.dialog-edit');
 
     $('.btn-delete').click(function (event) {
@@ -29,16 +31,17 @@
         btn.addEventListener('click', () => dialog.classList.add('hidden-edit'));
     });
 
-    document.querySelectorAll('.btn-edit-faqs').forEach(btn => {
-        const id = btn.closest('tr').querySelector('.no p').textContent;
-        const question = btn.closest('tr').querySelector('.Question p').textContent;
-        const answer = btn.closest('tr').querySelector('.Answer p').textContent;
+    for (i = 0; i < btnEdit.length; i++) {
+        btnEdit[i].addEventListener("click", function () {
+            const id = this.closest('tr').querySelector('.no p').textContent;
+            const question = this.closest('tr').querySelector('.Question p').textContent;
+            const answer = this.closest('tr').querySelector('.Answer p').textContent;
 
-        dialog.querySelector('#id').value = id;
-        dialog.querySelector('#question').value = question;
-        dialog.querySelector('#answer').value = answer;
-        btn.addEventListener('click', () => dialog.classList.remove('hidden-edit'));
+            dialog.querySelector('#id').value = id;
+            dialog.querySelector('#question').value = question;
+            dialog.querySelector('#answer').value = answer;
+            dialog.classList.remove('hidden-edit');
 
+        });
+    }
     });
-   
-});
